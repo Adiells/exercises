fd1f42bfe6ad049189f6d1a24052e652281a816d
Adiel Emilson <adielenilson@gmail.com>
2025-03-11 17:38:19 -0300

    aLista.js finalizado

--- eloquente/objetosEarrays/aLista.js
+++ eloquente/objetosEarrays/aLista.js
@@ -3,7 +3,28 @@ function arrayToList(array){
     for(let i = (array.length - 2); i > -1; i--){
         temp = prepend(array[i], temp)
     }
-    console.log(JSON.stringify(temp,null,2))
+    return temp
+}
+function listToArray(list){
+    let array = []
+    for(let i = list; i; i = i.rest){
+        array.push(i.Valor)
+
+    }
+    return array
+}
+function nth(list, number){
+    let count = 1
+    let valor = 0
+    for(let i = list; count <= number; i = i.rest){
+        if(i != null){
+            valor = i.Valor
+            count++
+        }else{
+            return undefined
+        }
+    }
+    return valor
 }
 function prepend(elemento, listad){
     let lista = {Valor: elemento, rest: listad}
