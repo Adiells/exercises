409b6b75fc2fe8fbd0b06ca413d8e3d4e36fb1f1
Adiel Emilson <adielenilson@gmail.com>
2025-03-13 21:10:27 -0300

    .

--- eloquente/objetosEarrays/comparacaoProdunda.js
+++ eloquente/objetosEarrays/comparacaoProdunda.js
@@ -1,32 +1,22 @@
-//esse eu não consegui :(
-//algum dia eu voltarei aqui para tentar novamente
-let c1 = []
-let c2 = []
-let count = 0
-function deepEqual(v1, v2){
-    let c = ''
-    if(typeof v1 == 'object' && v1 != null && 
-        typeof v2 == 'object' && v2 != null){
-            for(let i in v1){
-                if(typeof v1[i] == 'object' && v1[i] != null || typeof v2[i] == 'object' && v2[i] != null){
-                    return deepEqual(v1[i], v2[i])
-                }else{
-                    c1.push(v1[i], i)
-                    c2.push(v2[i], i)
-                    for(let a = 0; a < c1.length; a++){
-                        if(c1[a] === c2[a]){
-                            count++
-                            //console.log(count)
-                            console.log(c1.length)
-                        }
-                    }
-                }
-            }
-        }
-    if(count === c1.length){
+function deepEqual(v1, v2) {
+    if (v1 === v2) {
         return true
     }
-   else{
-        return false
+
+    if (typeof v1 === 'object' && v1 !== null && typeof v2 === 'object' && v2 !== null) {
+        let keys1 = Object.keys(v1)
+        let keys2 = Object.keys(v2)
+
+        if (keys1.length !== keys2.length){
+            return false
+        }
+
+        for (let key of keys1) {
+            if (!keys2.includes(key) || !deepEqual(v1[key], v2[key])) {
+                return false
+            }
+        }
+        return true
     }
+    return false
 }
